0ebc10be5a61029df7eeadc8ac531707aef9744d
rozi <rozi7724@gmail.com>
2024-01-20 15:08:46 +0200

    Product selection and order

---
+++ sign in.py
@@ -0,0 +1,90 @@
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_sign_in(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi20023@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi2002#")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+    time.sleep(2)
+
+
+def test_log_in_success(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi2001@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi200#")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+
+
+def test_log_in_via_password_6_char_success(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozalin@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi2#")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+
+
+def test_log_in_via_password_14_char_success(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi20@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi212223242!")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+
+
+def test_log_in_via_password_success(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi200@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi2002!")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+
+
+def test_log_in_via_yahoo_mail_success(setup):
+    driver = setup
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi2002@yahoo.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi20!!")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+    assert driver.find_element(By.XPATH, '//h5[text()="Combo Meal"]').is_displayed()
+
+
+def test_sign_in_without_email_should_alert(setup):
+    driver = setup
+    massage_error = 'Failed to log in'
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi200#")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+
+    # assert alert
+    wait = WebDriverWait(driver, 10)
+    wait.until(ec.alert_is_present())
+    alert = driver.switch_to.alert
+    assert massage_error in alert.text
+    alert.accept()
+
+
+def test_sign_in_invalid_different_password_should_alert(setup):
+    driver = setup
+    massage_error = 'Failed to log in'
+    driver.find_element(By.XPATH, '//a[@href="#/SignIn"]').click()
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your email"]').send_keys("rozi@gmail.com")
+    driver.find_element(By.XPATH, '//input[@placeholder ="Enter your password"]').send_keys("rozi20!!#")
+    driver.find_element(By.XPATH, '//button [@type ="submit"]').click()
+
+    # assert alert
+    wait = WebDriverWait(driver, 10)
+    wait.until(ec.alert_is_present())
+    alert = driver.switch_to.alert
+    assert massage_error in alert.text
+    alert.accept()
